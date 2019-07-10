8933a3d7bd4f98f1505aab4477916f58e97e053d
fengwei <36244881@qq.com>
2019-07-10 13:50:49 +0800

    element布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,59 +1,5 @@
 <template>
   <div class="home" style="height:100%">
-    <!-- <div style="height:50px;background:#545c64"></div>
-    <div
-      style="height:100%;position:relative;top:-50px;padding-top:50px;box-sizing:border-box;overflow:hidden;width:100%"
-    >
-      <div style="width:200px;height:100%;float:left;">
-        <el-row style="height:100%;background:#545c64">
-          <el-col :span="24">
-            <el-menu
-              default-active="2"
-              class="el-menu-vertical-demo"
-              @open="handleOpen"
-              @close="handleClose"
-              background-color="#545c64"
-              text-color="#fff"
-              active-text-color="#ffd04b"
-            >
-              <el-submenu index="1">
-                <template slot="title">
-                  <i class="el-icon-location"></i>
-                  <span>导航一</span>
-                </template>
-                <el-menu-item-group>
-                  <template slot="title"
-                    >分组一</template
-                  >
-                  <el-menu-item index="1-1">选项1</el-menu-item>
-                  <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                  <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                  <template slot="title"
-                    >选项4</template
-                  >
-                  <el-menu-item index="1-4-1">选项1</el-menu-item>
-                </el-submenu>
-              </el-submenu>
-              <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
-              </el-menu-item>
-              <el-menu-item index="3">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航三</span>
-              </el-menu-item>
-            </el-menu>
-          </el-col>
-        </el-row>
-      </div>
-      <div style="overflow:hidden;height:100%;position:relative">
-        <router-view :dataMethod="dataMethod"></router-view>
-      </div>
-    </div> -->
     <el-container class="content">
       <el-header></el-header>
       <el-container>
@@ -72,7 +18,7 @@
                 <el-submenu index="1">
                   <template slot="title">
                     <i class="el-icon-location"></i>
-                    <span>导航一</span>
+                    <span>LayOut布局</span>
                   </template>
                   <el-menu-item-group>
                     <template slot="title"
@@ -152,7 +98,7 @@ export default {
 .el-aside {
   background-color: #d3dce6;
   color: #333;
-  text-align: center;
+  text-align: left;
   line-height: 200px;
 }
 
